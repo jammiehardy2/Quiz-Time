5dc3643fd2794a19eaf7fb937c7774cf2cef9a7a
Jammie <73665246+jammiehardy2@users.noreply.github.com>
2020-12-08 20:26:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+ //User input variables 
+ //AS A coding boot camp student
+ var test = document.getElementById9("Test");
+ var questions = document.getElementById("questions");
+ var choice = document.getElementById("choice");
+ var answer1 = document.getElementById("a4");
+ var answer2 = document.getElementById("a3");
+ var answer3 = document.getElementById("a1");
+ var answer4 = document.getElementById("a2");
+ var score=0;
+ var totalTime=0;
+ var nextQuestion = 0;
+ var timeInt;
+ var countdown = document.getElementById("time left");
+ var setTimer = document.getElementById("timer");
+ var time = document.getElementById("answerCountTime");
+ var testTimer= 
+ //var confirm = document("correct");
+ //var startTest = document.getElementById("startTimer");
+ //var questionForm= form.getElementById("formquestions");
+
+container.addEventListener("click",start);
+function start();
+if (totalSeconds > 0){
+        interval =setInterval(function()
+        time++);
+3000);
+
+    }
+
+function totalTime(){
+countdown();
+
+
+ //I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+ // Make 4 questions in an Array 
+ form= question ()
+    
+     
+    question: "How many hours should we be doing homework each week?",
+    answer1 = "5",
+    answer2 = "10",
+     a3: "15",
+     a4: "20",
+     correct: "a4",
+     if (confirm)answer1= a4) {
+        // Alert user count won or lost
+        alert("you are correct" ++,10)
+    }
+ }
+    {
+        question: "Where do we do load gitpull onto our machines?",
+        a1: "github",
+        a2: "bootstrap",
+        a3: "gitbash",
+        a4: "gitfind",
+        correct: "a3",
+        if (confirm)answer2= a3) {
+           // Alert user count won or lost
+            alert("you are correct" ++,10)
+       }
+      
+     {
+        question: "Where do I find my grades?",
+        a1: "bootcampspot",
+        a2: "vs",
+        a3: "gitlab",
+        a4: "microsoftoffice",
+        correct: "a1",
+        if (confirm)answer3= a1) {
+            // Alert user count won or lost
+            alert("you are correct" ++,10)
+       }
+
+    {
+        question: "Where is everything saved right before submitting?",
+        a1: "vs",
+        a2: "github",
+        a3: "mydocuments",
+        a4: "gitlab",
+        correct: "a2",
+        if (confirm)answer4= a2) {
+            // Alert user count won or lost
+
+            alert("you are correct" ++,10)
+       }
+ ]
+
+ 
+ //SO THAT I can gauge my progress compared to my peers
+
+
